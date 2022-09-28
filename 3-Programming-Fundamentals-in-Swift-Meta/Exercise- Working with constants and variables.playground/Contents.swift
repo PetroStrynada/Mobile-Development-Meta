66cba8dd829e2bc57242a02d48b4c3e0b9f3fc67
Petro Strynada <petro.strynada@gmail.com>
2022-09-28 19:58:27 +0300

    Added 3-Programming-Fundamentals-in-Swift-Meta Did lab Exercise- Working with constants and variables

---
+++ 3-Programming-Fundamentals-in-Swift-Meta/Exercise- Working with constants and variables.playground/Contents.swift
@@ -0,0 +1,14 @@
+let day = "Monday"
+let dailyTemperature = 75
+print("Today is \(day). Rise and shine")
+print("The temperature on \(day) is \(dailyTemperature) °F")
+
+var temperature = 70
+print("The temperature on \(day) morning is \(temperature)")
+
+temperature = 80
+print("The temperature on \(day) evening is \(temperature)")
+
+let weeklyTemperature = 75
+temperature = weeklyTemperature
+print("The average week temperature is \(temperature) °F")
