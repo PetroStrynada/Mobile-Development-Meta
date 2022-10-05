6cc27514a9f24f1892b21ca86d3100fc9687d427
Petro Strynada <petro.strynada@gmail.com>
2022-10-05 15:11:01 +0300

    Exercise: Dictionaries in Swift DONE
    var newDictionary: <String, Int>() - new empty dictionary
    newDictionary = [
    		"first" = 1,
    		"second" = 2
    		] - adding new values to dictionary
    newDictionary["first"] = nil
    newDictionary.removeValue(forKey: "second") - removing
    newDictionary = [:] - reset to empty dictonary
    newDictionary["first"] = 10 - remove 1 with 10
    newDictionary.updateValue(90, forKey: "first") - remove 10 with 90
    newDictionary["first"]! += 10 - adding 10 to curent "first" value 90 + 10
    ! needs to unwrapneeds to unwrap optional value of Int stored in
    dictionary

---
+++ 3-Programming-Fundamentals-in-Swift-Meta/Exercise- Dictionaries in Swift.playground/Contents.swift
@@ -0,0 +1,111 @@
+//https://www.coursera.org/learn/programming-fundamentals-swift/supplement/qquRr/exercise-dictionaries-in-swift
+
+/*
+var weeklyTemperature = [String: Int]()
+//var weeklyTemperature = Dictionary <String: Int>()
+weeklyTemperature = [
+                    "Monday": 70,
+                    "Tuesday": 75
+                    ]
+print(weeklyTemperature)
+weeklyTemperature.updateValue(80, forKey: "Wednesday")
+weeklyTemperature.updateValue(85, forKey: "Thursday")
+print(weeklyTemperature)
+weeklyTemperature["Friday"] = 90
+print(weeklyTemperature)
+weeklyTemperature["Saturday"] = 95
+print("\(weeklyTemperature)")
+
+weeklyTemperature["Monday"]! += 20
+print("The temperature on Monday \(weeklyTemperature["Monday"]!) F")
+
+
+if let temperature = weeklyTemperature["Sunday"] {
+    
+} else {
+    weeklyTemperature["Sunday"] = 100
+}
+print("The temperature on Sunday \(weeklyTemperature["Sunday"]!) F")
+
+if weeklyTemperature.count == 7 {
+    print("You have access to the weather forecast of the whole week")
+    weeklyTemperature = [:]
+    print("Reset weekly temperatures for next week!")
+    print(weeklyTemperature)
+}
+*/
+
+
+
+//Second repetition
+var weeklyTemperature: Dictionary <String, Int> = [
+    "Monday": 70,
+    "Tuesday": 75,
+    "Wednesday": 80,
+    "Thursday": 85,
+    "Friday": 90,
+    "Saturday": 95,
+    "abc": 123,
+    "dfj": 456,
+]
+weeklyTemperature.removeValue(forKey: "abc")
+weeklyTemperature["dfj"] = nil
+print(weeklyTemperature)
+print("The temperature o Monday \(weeklyTemperature["Monday"]! + 30) F")
+print("\(weeklyTemperature["Monday"]!)")
+print("Value 'abc' after .removeValue ''\(weeklyTemperature["abc"])'', value 'dfj' after = nil ''\(weeklyTemperature["dfj"])''")
+
+
+weeklyTemperature.updateValue(90, forKey: "Monday")
+print("\(weeklyTemperature["Monday"]!)")
+weeklyTemperature["Monday"]! -= 20
+print("\(weeklyTemperature["Monday"]!)")
+weeklyTemperature["Monday"]! += 20
+print("The temperature on Monday is \(weeklyTemperature["Monday"]!) F")
+
+
+if let temperature = weeklyTemperature["Sunday"] {
+
+} else {
+    weeklyTemperature["Sunday"] = 100
+}
+print("The Sunday temperature is \(weeklyTemperature["Sunday"]!) F")
+
+func resetTheWeatherForecastStatus() {
+    if weeklyTemperature.count == 7 {
+        print("""
+The weather forecast for a week:
+On Monday \(weeklyTemperature["Monday"]!) F
+On Tuesday \(weeklyTemperature["Tuesday"]!) F
+On Wednesday \(weeklyTemperature["Wednesday"]!) F
+On Thursday \(weeklyTemperature["Thursday"]!) F
+On Friday \(weeklyTemperature["Friday"]!) F
+On Saturday \(weeklyTemperature["Saturday"]!) F
+On Sunday \(weeklyTemperature["Sunday"]!) F
+""")
+        weeklyTemperature = [:]
+    }
+}
+
+
+resetTheWeatherForecastStatus()
+
+
+weeklyTemperature = [
+    "Monday": 90,
+    "Tuesday": 60,
+    "Wednesday": 40,
+    "Thursday": 50,
+    "Friday": 70,
+    "Saturday": 80,
+    "Sunday": 110
+]
+weeklyTemperature["Monday"] = 1000
+print("\(weeklyTemperature["Monday"])")
+print("\(weeklyTemperature["Sunday"])")
+
+
+resetTheWeatherForecastStatus()
+
+weeklyTemperature = ["first": 1]
+print("\(weeklyTemperature)")
